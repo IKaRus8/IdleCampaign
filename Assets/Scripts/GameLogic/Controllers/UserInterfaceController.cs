1f542ab989ddd86d50008ba0bbc29a618d991336
astoriyavf2001@gmail.com <astoriyavf2001@gmail.com>
2023-10-12 18:23:00 +0300

    Created a user interface in the form of a UI Toolkit. Connected the package UnityMVVMToolkit.

---
+++ Assets/Scripts/GameLogic/Controllers/UserInterfaceController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UIElements;
+
+
+namespace GameLogic.Controllers
+{
+    public class UserInterfaceController : MonoBehaviour
+    {
+       
+    }
+}
